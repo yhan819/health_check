740ca5196184457da0c71b0758122e3ab911aa8d
Yoon-Suk (John) Han <yhan819@stanford.edu>
2013-06-28 15:05:09 -0700

    checking the learning server;

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
+import sys
+sys.path.append('gen_py')
+
+from datetime import datetime
+from healthservice import HealthCheckService
+from healthservice.ttypes import *
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
 app = Flask(__name__)
+
+############################# Calls ################################
+
+
+@app.route('/test')
+def test():
+  client.healthCheck()
+  return json.dumps({"message":"testing"})
+
+
+
+
+########################### running the app ########################
 
 @app.route("/")
 def hello():
-  return "Hello World"
+  return render_template('index.html')
 
 if __name__ == "__main__":
-  app.run()
+  local = "localhost"
+  local_port = "5678"
+  real_host = "50.17.210.180"
+  real_port = "5001"
+  try:
+    transport = TSocket.TSocket(real_host, real_port)
+    transport = TTransport.TBufferedTransport(transport)
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    client = HealthCheckService.Client(protocol)
+    transport.open()
+    app.run()
+    transport.close()
+  except:
+    print "one of the server is not running"
